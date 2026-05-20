ecf271fa371882bbba1f8a9f26261d899cc81e5d
quratulain-hue <ggcquratulainbsit1@gmail.com>
2026-05-20 19:45:46 +0500

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+private:
+    double num1, num2, result;
+    char op;
+    
+public:
+    void getInput() {
+        cout << "Enter first number: ";
+        cin >> num1;
+        
+        cout << "Enter operator (+, -, *, /): ";
+        cin >> op;
+        
+        cout << "Enter second number: ";
+        cin >> num2;
+    }
+    
+    void calculate() {
+        if (op == '+') {
+            result = num1 + num2;
+            displayResult();
+        }
+        else if (op == '-') {
+            result = num1 - num2;
+            displayResult();
+        }
+        else if (op == '*') {
+            result = num1 * num2;
+            displayResult();
+        }
+        else if (op == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+                displayResult();
+            } else {
+                cout << "Error: Cannot divide by zero" << endl;
+            }
+        }
+        else {
+            cout << "Invalid operator" << endl;
+        }
+    }
+    
+    void displayResult() {
+        cout << "Result: " << result << endl;
+    }
+};
+
+int main() {
+    Calculator calc;
+    
+    calc.getInput();
+    calc.calculate();
+    
+    return 0;
+}
